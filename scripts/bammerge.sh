24b716d3769a2efa12505151ffb291d5d7a33210
Elias Oziolor <oziolor@gmail.com>
2017-07-03 09:53:49 -0500

    import 2: indexed and merged ind bam files, counted coverage per base

---
+++ scripts/bammerge.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+bamtools merge \
+-list /home/oziolore/restoreFromData/fhet/data/align/merged_bams.list -out /home/oziolore/restoreFromData/fhet/data/align/all_merged.bam
---
+++ scripts/list_bam.sh
@@ -0,0 +1,1 @@
+echo /home/oziolore/restoreFromData/fhet/data/align/*.bam | xargs -n1 > /home/oziolore/restoreFromData/fhet/data/align/merged_all.bam
