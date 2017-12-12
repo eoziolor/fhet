ccb8bf720746669ce7fbe55c1f49420162b41812
Elias Oziolor <oziolor@gmail.com>
2017-12-12 13:29:49 -0600

    looking at haplotypes at shared/res only/interm only sites (z outliers). Extracted with tabix. Feeding into .r script outside of cluster.

---
+++ scripts2/haplo/shared_vcf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=01:00:00
+
+#programs & files
+my_tabix=/data/oziolore/program/htslib/tabix
+my_bgz=/data/oziolore/program/htslib/bgzip
+
+my_vcf=/data/oziolore/fhet/data/varcall/filtered_chr.vcf.bgz
+my_list=/data/oziolore/fhet/data/outliers/zshared_formatted
+my_out=/data/oziolore/fhet/data/outliers/zshared.vcf.bgz
+
+$my_tabix -fh $my_vcf chr1:0-1 | $my_bgz > $my_out
+xargs -a $my_list -I {} $my_tabix -f $my_vcf {} | $my_bgz >> $my_out
