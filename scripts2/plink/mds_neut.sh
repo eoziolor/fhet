b5e58076e407345d08bd1dbea7e2257d3eca8db0
Elias Oziolor <oziolor@gmail.com>
2017-12-12 18:51:03 -0600

    Worked on haplotypes of unique outliers in populations. In addition, figured out neutral regions and mds analysis of those

--- scripts2/plink/mds_neut.sh
+++ scripts2/plink/mds_neut.sh
@@ -2,14 +2,12 @@
 #PBS -l nodes=1:ppn=8
 #PBS -l walltime=72:00:00
 
-my_vcf=/data/oziolore/fhet/data/varcall/filtered_chr.vcf.bgz
+my_vcf=/data/oziolore/fhet/data/varcall/filtered_neutral.vcf.bgz
 my_plink=/data/oziolore/program/plink/plink
-my_tabix=/data/oziolore/program/htslib/tabix
-my_regions=/data/oziolore/fhet/data/plink/list_neut_final.txt
 my_out=/data/oziolore/fhet/data/plink/
 
 $my_plink \
--vcf <(xargs -a $my_regions -I {} $my_tabix -fh $my_vcf)  \
+-vcf $my_vcf  \
 --allow-extra-chr \
 --cluster \
 --mds-plot 5 \
