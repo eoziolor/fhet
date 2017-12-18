91b4842030b4c50a72328a1b411a1eae47ceeb51
Elias Oziolor <oziolor@gmail.com>
2017-12-18 16:32:02 -0600

    created vcf for random set of genes the same size as shared outliers. In addition worked out vcf for intermediates, but for the region selection type

---
+++ scripts2/haplo/random_vcf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=00:00:10
+
+#programs & files
+my_tabix=/data/oziolore/program/htslib/tabix
+my_bgz=/data/oziolore/program/htslib/bgzip
+
+my_vcf=/data/oziolore/fhet/data/varcall/filtered_chr.vcf.bgz
+my_list=/data/oziolore/fhet/data/outliers/random_483win_formatted.bed
+my_out=/data/oziolore/fhet/data/outliers/random_483win.vcf.bgz
+
+$my_tabix -fh $my_vcf chr1:0-1 | $my_bgz > $my_out
+xargs -a $my_list -I {} $my_tabix -f $my_vcf {} | $my_bgz >> $my_out
