1174d0715879b2fbd0a639427230b9b0127eda52
Elias Oziolor <oziolor@gmail.com>
2017-12-19 12:33:17 -0600

    removed chromosome1 from mds to see if that's driving distances

---
+++ scripts2/haplo/shared_vcf_10no1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=01:00:00
+
+#programs & files
+my_tabix=/data/oziolore/program/htslib/tabix
+my_bgz=/data/oziolore/program/htslib/bgzip
+
+my_vcf=/data/oziolore/fhet/data/varcall/filtered_chr.vcf.bgz
+my_list=/data/oziolore/fhet/data/outliers/zshared_10perregion_no1_formatted.bed
+my_out=/data/oziolore/fhet/data/outliers/zshared_10_no1.vcf.bgz
+
+$my_tabix -fh $my_vcf chr1:0-1 | $my_bgz > $my_out
+xargs -a $my_list -I {} $my_tabix -f $my_vcf {} | $my_bgz >> $my_out
