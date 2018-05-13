a3647f74d410fe85690980983413f3b798a7e4d5
Elias Oziolor <oziolor@gmail.com>
2018-05-13 15:13:03 -0500

    folded subsampled theta estimates finished.

---
+++ scripts2/dosaf/6.thetas_unfolded.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=8
+#PBS -J 1-7
+
+pops=BB\ VB\ PB\ SJ\ BNP\ SP\ GB
+
+one=$(echo $pops | cut -f $PBS_ARRAY_INDEX -d ' ')
+
+#files and program
+
+my_angsd=/data/oziolore/program/angsd_norm/angsd
+list=/data/oziolore/fhet/data/list2/$one\2\_new.txt
+my_sfs=/data/oziolore/fhet/data/angsd2/$one\_unfolded\.sfs
+genome=/data/oziolore/fhet/data/genome2/unsplit_merge.fasta
+my_keep=/data/oziolore/fhet/data/angsd2/keepsites2.file
+outdir=/data/oziolore/fhet/data/angsd2/theta
+outfile=$one\_unfolded\.theta
+
+#script
+
+$my_angsd \
+-bam $list \
+-out $outdir/$outfile \
+-doThetas 1 \
+-doSaf 1 \
+-pest $my_sfs \
+-anc $genome \
+-fold 1 \
+-GL 2 \
+-minMapQ 30 \
+-minQ 20 \
+-minind 10 \
+-P 8 \
+-sites $my_keep
+
---
+++ scripts2/dosaf/subsample/5.theta_window_sub.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=02:00:00
+#PBS -J 1-7
+
+pops=BB\ VB\ PB\ SJ\ BNP\ SP\ GB
+
+one=$(echo $pops | cut -f $PBS_ARRAY_INDEX -d ' ')
+
+#programs and files
+
+my_bedtools=/data/oziolore/program/bedtools2/bin/bedtools
+thetas=/data/oziolore/fhet/data/angsd2/theta/subsample/$one\_readable_theta.gz
+window=/data/oziolore/fhet/data/windows2/new_noah.1kb.bed
+my_genome=/data/oziolore/fhet/data/genome2/unsplit_merge.fasta.fai
+outdir=/data/oziolore/fhet/data/angsd2/theta/subsample
+outfile=$one\_neut_1kb.bed
+
+zcat $thetas | \
+egrep -v "^#" | \
+awk '{OFS="\t"}{w=exp($3)}{pi=exp($4)}{s=$2-1}{print $1,s,$2,w,pi}' | \
+$my_bedtools map \
+-a $window \
+-b stdin \
+-g <(cut -f 1-2 $my_genome) \
+-c 4,4,5,5 \
+-o sum,count,sum,count > $outdir/$outfile
