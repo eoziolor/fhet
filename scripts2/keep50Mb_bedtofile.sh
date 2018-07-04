423d5a800e6deb7806b05b4348ef0eab4eba0ca7
Elias Oziolor <oziolor@gmail.com>
2018-07-03 22:28:34 -0500

    estimated outlier regions with sum statistics and took them out in vcfs to plot

--- scripts2/keep50Mb_bedtofile.sh
+++
@@ -1,2 +0,0 @@
-cat /home/oziolore/restoreFromData/fhet/data/angsd2/keep50Mb2.bed | \
-awk '{OFS="\t"}{s=$2+1}{print $1,s,$3}' > /home/oziolore/restoreFromData/fhet/data/angsd2/keep50Mb2.file
--- scripts2/keepsites_bedtofile.sh
+++
@@ -1,2 +0,0 @@
-cat /home/oziolore/restoreFromData/fhet/data/angsd2/keepsites2.bed | \
-awk '{OFS="\t"}{s=$2+1}{print $1,s,$3}' > /home/oziolore/restoreFromData/fhet/data/angsd2/keepsites2.file
